0814bb2085e514c188f3b4dfc001c9da686b3771
Peter Grigor <petrupewdiepie@gmail.com>
2025-07-23 16:18:57 +0300

    some chnages

--- ApiControllers/ConfigController.cs
+++ ApiControllers/ConfigController.cs
@@ -8,24 +8,9 @@ namespace QwenChatBackend.ApiControllers;
 [Route("api/config")]
 public class ConfigController : ControllerBase
 {
-    private readonly ILogService _logService;
-    private readonly IConfiguration _configuration;
-
-    public ConfigController(ILogService logService, IConfiguration configuration)
-    {
-        _logService = logService;
-        _configuration = configuration;
-    }
-
     [HttpGet("base-url")]
     public async Task<IActionResult> GetBaseUrl()
     {
-        await _logService.LogAsync(new Log
-        {
-            Method = "GET",
-            Endpoint = "/api/config/base-url"
-        });
-
         var baseUrl = "https://veo3-backend-118847640969.europe-west1.run.app";
 
         var response = new
